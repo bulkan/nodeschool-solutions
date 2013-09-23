31f463f429a3774face9acc081aa4fc7acd8203f
Bulkan Evcimen <bulkan@gmail.com>
2013-09-23 22:51:52 +1000

    got the the pipe ordering wrong :(

--- stream-adventure/crypt.js
+++ stream-adventure/crypt.js
@@ -4,4 +4,4 @@ var pass = process.argv[2];
 
 var stream = crypto.createDecipher('aes256', process.argv[2]);
 
-stream.pipe(process.stdin).pipe(process.stdout);
+process.stdin.pipe(stream).pipe(process.stdout);
